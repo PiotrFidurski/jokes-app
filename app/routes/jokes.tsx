5eabb67fa13c4b1d4494917ab99eb417e4e52973
Piotr Fidurski <crunor55@hotmail.com>
2021-12-19 12:57:50 +0100

    get jokes data from db

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,11 @@
-import type { LinksFunction } from "remix";
-import { Link, Outlet } from "remix";
+import {
+  Link,
+  LinksFunction,
+  LoaderFunction,
+  Outlet,
+  useLoaderData,
+} from "remix";
+import { db } from "~/utils/db.server";
 import stylesUrl from "../styles/jokes.css";
 
 export const links: LinksFunction = () => {
@@ -11,7 +17,23 @@ export const links: LinksFunction = () => {
   ];
 };
 
+type LoaderData = { jokes: Array<{ id: string; name: string }> };
+
+export let loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokes: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  };
+
+  return data;
+};
+
 export default function JokesRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -30,9 +52,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data.jokes.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,28 @@
+import { Joke } from "@prisma/client";
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+
+export let loader: LoaderFunction = async ({ params }) => {
+  const jokeId = params.jokeId;
+
+  const joke = await db.joke.findUnique({ where: { id: jokeId } });
+  if (!joke) throw new Error("Joke not found");
+
+  const data: LoaderData = { joke };
+
+  return data;
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData();
+
   return (
     <div>
-      <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>Here's your hilarious j oke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
